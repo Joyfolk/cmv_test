3ecd755ba5b3111a2489c1c9c1fe42e8cf277a7d
Mikhail Turnovskiy <joyfolk@gmail.com>
2020-05-23 13:41:16 +0300

    task 2 completed

--- task-2/src/main/scala/MultiQueue.scala
+++ task-2/src/main/scala/MultiQueue.scala
@@ -1,27 +1,74 @@
 package task2
+import java.util.concurrent.atomic.{AtomicInteger, AtomicIntegerArray}
+
+import scala.annotation.tailrec
+import scala.util.Random
 
 class MultiQueue[A >: Null] private (
   // nQueues is guaranteed to be at least 2
-  nQueues: Int
+  nQueues: Int,
   // other arguments here if needed
+  rng: Random = new Random() // would be thread contention point in real app, replace with threadlocal instances or something like that
   )(implicit ordering: Ordering[A]) {
 
-  // XXX: You'll need ordering but it's not referenced anywhere and the compiler, rightly, complains about
-  // that. You can safely delete this method when you're done with the class.
-  def xxxRemoveMe = ordering
+  private val queues: Array[Queue[A]] = Array.fill(nQueues)(Queue.empty)
+  private val locks: AtomicIntegerArray = new AtomicIntegerArray(nQueues)
+  private val _size: AtomicInteger = new AtomicInteger(0)
 
-  def isEmpty: Boolean = ???
-  def size: Int        = ???
+  def isEmpty: Boolean = size == 0
+  def size: Int        = _size.get()
 
   def insert(element: A): Unit = {
-    ???
+    withRandomLock { n =>
+      queues(n).enqueue(element)
+      _size.incrementAndGet()
+      ()
+    }
+  }
+
+  @tailrec
+  private def withRandomLock[T](f: Int => T): T =
+    acquireAndDo(rng.nextInt(nQueues))(f) match {
+      case Some(x) => x
+      case None    => withRandomLock(f)
+    }
+
+  private def acquireAndDo[T](n: Int)(f: Int => T): Option[T] =
+    if (locks.compareAndSet(n, 0, 1))
+      try Some(f(n)) finally locks.set(n, 0)
+    else
+      None
+
+  @tailrec
+  private def with2RandomLocks[T](f: (Int, Int) => T): T = {
+    val n = (rng.nextInt(nQueues), rng.nextInt(nQueues))
+    if (n._1 == n._2)
+      with2RandomLocks(f)
+    else {
+      val m = if (n._1 < n._2) n else n.swap
+      acquireAndDo(m._1) { n1 =>
+        acquireAndDo(m._2) { n2 =>
+          val r = f(n1, n2)
+          if (r != null)
+            _size.decrementAndGet()
+          r
+        }
+      }.flatten match {
+        case Some(v) => v
+        case None => with2RandomLocks(f)
+      }
+    }
   }
 
   /*
    * Smallest elements (non-strictly) first.
    */
-  def deleteMin(): A = {
-    ???
+  def deleteMin(): A = with2RandomLocks { (n1, n2) =>
+    if (queues(n1).peekMin != null && (queues(n2).peekMin == null
+      || queues(n2).peekMin != null && ordering.compare(queues(n1).peekMin, queues(n2).peekMin) <= 0))
+      queues(n1).deleteMin()
+    else
+      queues(n2).deleteMin()
   }
 }
 
--- task-2/src/test/scala/task2/MultiQueueTest.scala
+++ task-2/src/test/scala/task2/MultiQueueTest.scala
@@ -2,8 +2,93 @@ package task2
 
 import org.scalatest.{FreeSpec, MustMatchers}
 
+import scala.annotation.tailrec
+
 class MultiQueueTest extends FreeSpec with MustMatchers {
-  "trivial" in {
-    true mustBe true
+  "empty queue isEmpty" in {
+    val q = MultiQueue.empty[Integer](2)
+    q.isEmpty mustBe true
+  }
+
+  "empty queue deleteMin must return nulls" in {
+    val q = MultiQueue.empty[Integer](2)
+    q.deleteMin() mustBe null
+    q.deleteMin() mustBe null
+  }
+
+  "queue insert should work and deleteMin with single value must return that value" in {
+    val q = MultiQueue.empty[Integer](2)
+    q.insert(5)
+    q.deleteMin() mustBe 5
+  }
+
+  "queue with nQueue number two should always return minimum value" in {
+    val q = MultiQueue.empty[Integer](2)
+    q.insert(5)
+    q.insert(3)
+    q.insert(1)
+    q.insert(7)
+    q.insert(9)
+    q.insert(11)
+    q.insert(13)
+    q.deleteMin() mustBe 1
+  }
+
+  "size should always be correct" in {
+    val q = MultiQueue.empty[Integer](2)
+    q.size mustBe 0
+    q.insert(5)
+    q.insert(3)
+    q.size mustBe 2
+    q.insert(1)
+    q.insert(7)
+    q.insert(9)
+    q.size mustBe 5
+    q.insert(11)
+    q.insert(13)
+    q.size mustBe 7
+    q.deleteMin()
+    q.size mustBe 6
+    q.deleteMin()
+    q.deleteMin()
+    q.deleteMin()
+    q.deleteMin()
+    q.deleteMin()
+    q.size mustBe 1
+    q.deleteMin()
+    q.size mustBe 0
+    q.deleteMin()
+    q.deleteMin()
+    q.deleteMin()
+    q.size mustBe 0
+  }
+
+  "queue should work with more than 2 queues" in {
+    val q = MultiQueue.empty[Integer](5)
+    val toAdd = Range(0, 10)
+    @tailrec
+    def takeN(n: Int, s: Set[Int] = Set()): Set[Int] =
+      if (n == 0)
+        s
+      else if (q.isEmpty)
+        throw new IllegalStateException("empty queue")
+      else {
+        val e = q.deleteMin()
+        if (e != null)
+          takeN(n - 1, s + e)
+        else
+          takeN(n, s)
+      }
+    toAdd.foreach(q.insert(_))
+    q.size mustBe 10
+    var r = takeN(5)
+    q.size mustBe 5
+    r ++= takeN(3)
+    q.size mustBe 2
+    q.isEmpty mustBe false
+    r ++= takeN(2)
+    q.size mustBe 0
+    q.isEmpty mustBe true
+    r mustBe toAdd.toSet
   }
 }
